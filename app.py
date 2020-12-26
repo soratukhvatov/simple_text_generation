eb24173c5d18239691e0504d30add0484d2192f9
soratukhvatov <74349898+soratukhvatov@users.noreply.github.com>
2020-12-26 16:35:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def load_model():
 
     list_files = subprocess.run(["ls", "mymodel","-la"])
     print("The exit code was: %d" % list_files.returncode)
-    return pipeline("text-generation", model="gpt-2")
+    return pipeline("text-generation", model="gpt2")
 
 
 model = load_model()
