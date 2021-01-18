c465261a1c270753079de025ffb7d2c3f698cc55
soratukhvatov <soratukhvatov@soramitsu.co.jp>
2021-01-18 12:08:04 +0300

    added options for manipulating text generation

--- app.py
+++ app.py
@@ -1,48 +1,112 @@
 
 import urllib
 import streamlit as st
+import string
 import torch
 from transformers import pipeline
 from pathlib import Path
 import subprocess
 
-list_files = subprocess.run(["ls", "-l"])
-print("The exit code was: %d" % list_files.returncode)
-
-list_files = subprocess.run(["ls", "mymodel","-la"])
-print("The exit code was: %d" % list_files.returncode)
+# list_files = subprocess.run(["ls", "-l"])
+# print("The exit code was: %d" % list_files.returncode)
+#
+# list_files = subprocess.run(["ls", "test-clm-tolkin", "-la"])
+# print("The exit code was: %d" % list_files.returncode)
+from mytextgenerationpipeline import run_my_pplm
+from transformers import GPT2Tokenizer
 
 @st.cache(allow_output_mutation=True, suppress_st_warning=True)
 def load_model():
-    cloud_model_location = "1bNaZemZ88E1arDOiSTRWFlsRw_QaiPu9"
+    # cloud_model_location = "1bNaZemZ88E1arDOiSTRWFlsRw_QaiPu9"
+    #
+    # f_checkpoint = Path("mymodel/pytorch_model.bin")
+    # x = 0
+    # if not f_checkpoint.exists():
+    #     with st.spinner("Downloading model... this may take awhile! \n Don't stop it!"):
+    #         from GD_download import download_file_from_google_drive
+    #         download_file_from_google_drive(cloud_model_location, f_checkpoint)
 
-    f_checkpoint = Path("mymodel/pytorch_model.bin")
-    x = 0
-    if not f_checkpoint.exists():
-        with st.spinner("Downloading model... this may take awhile! \n Don't stop it!"):
-            from GD_download import download_file_from_google_drive
-            download_file_from_google_drive(cloud_model_location, f_checkpoint)
-
-    list_files = subprocess.run(["ls", "mymodel","-la"])
-    print("The exit code was: %d" % list_files.returncode)
-    return pipeline("text-generation", model="gpt2")
+    # list_files = subprocess.run(["ls", "mymodel", "-la"])
+    # print("The exit code was: %d" % list_files.returncode)
 
 
-model = load_model()
+    # return pipeline("text-generation", model="mygpt2-medium")
+    from transformers.modeling_gpt2 import GPT2LMHeadModel
+    from transformers import AutoTokenizer
 
-textbox = st.text_area('Start your story:', '', height=200, max_chars=1000)
-slider = st.slider('Max story length (in characters)', 50, 200)
+    # model = GPT2LMHeadModel.from_pretrained("mygpt2-medium")
+    # tokenizer = AutoTokenizer.from_pretrained("mygpt2-medium")
+    # >> > pipeline('ner', model=model, tokenizer=tokenizer)
+    # load pretrained model
+    pretrained = "mygpt2-medium"
+    model = GPT2LMHeadModel.from_pretrained(
+        pretrained,
+        output_hidden_states=True
+    )
+    tokenizer = GPT2Tokenizer.from_pretrained(pretrained)
+
+    return model, tokenizer#pipeline("text-generation", model="mygpt2-medium")#, tokenizer=tokenizer)#, config="mygpt2-medium")
+
+
+model, tokenizer = load_model()
+st.title('Text generation')
+st.subheader(""" "The GPT-2 MEDIUM" model is fine-tuned on articles from Medium.com .\n
+The following tags were selected: 
+* Blockchain 
+* Cryptocurrency 
+* Polkadot 
+* Sora""")
+textbox = st.text_area('Start your story:', 'The original Blockchain is open-source technology which ',
+                       height=200, max_chars=1000)
+slider = st.slider('Max story length (in characters)', 50, 200, 140)
 button = st.button('Generate')
-x=0
 
+listoftopics = ['legal', 'military', 'monsters', 'politics', 'religion', 'science', 'space', 'technology']
+discriminators = ['clickbait', 'non clickbait', 'positive sentiment', 'neg sentiment']
+
+topics = st.sidebar.radio("Select a Topic", listoftopics)
+# step_size = st.sidebar.slider('Step size', 0.01, 0.1, 0.05)
+# kl_scale = st.sidebar.slider('KL-scale', 0.0, 1.0)
+# gm_scale = st.sidebar.slider('GM-scale', 0.0, 1.0, 0.95)
+# num_iterations = st.sidebar.slider('Num iterations (impacts gen. time)', 1, 10, 2)
+# gen_length = st.sidebar.slider('Gen. length (impacts gen. time)', 5, 20)
+# is_sampling = st.sidebar.checkbox('Use sampling')
+
+c1, c2 = st.sidebar.beta_columns(2)
+rand_seed = c1.number_input('Random Seed', value=0)
+
+# st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
+# x=0
+# print(topics)
+# 0/0
 if button:
-    # output_text = mymodel(textbox, max_length=slider)[0]['generated_text']
-    output_text = model(textbox, do_sample=True, max_length=slider, top_k=50, top_p=0.95, num_returned_sequences=1)[0][
-        'generated_text']
+    # output_text = model(textbox, max_length=slider)[0]['generated_text']
+
+    output_text = run_my_pplm(model, #"C:/Users/Admin/simple_text_generation/mygpt2-medium",
+                                  tokenizer,
+                                  cond_text=textbox,
+                                  bag_of_words=topics,
+                                  length=slider,
+                                  uncond=False,
+                                    verbosity='quiet',
+                              colorama=True,
+                              seed=rand_seed)
+
+    # print(output_text)
+    output_text = output_text[0][-1]
+
+    # output_text = model(textbox, do_sample=True, max_length=slider, top_k=50, top_p=0.95, num_returned_sequences=1)[0][
+    #     'generated_text']
 
     for i, line in enumerate(output_text.split("\n")):
-        if ":" in line:
-            speaker, speech = line.split(':')
-            st.markdown(f'__{speaker}__: {speech}')
-        else:
-            st.markdown(line)
+        # if False and ":" in line:
+        #     speaker, speech = line.split(':')
+        #     st.markdown(f'__{speaker}__: {speech}')
+        # else:
+            line = ''.join([str(char) for char in line if char in string.printable])
+            st.markdown(line, unsafe_allow_html=True)
+# model(textbox, max_length=slider)[0]['generated_text']
+
+
+
+# print(generated_texts)
---
+++ mytextgenerationpipeline.py
@@ -0,0 +1,338 @@
+import sys
+
+# insert at 1, 0 is the script path (or '' in REPL)
+sys.path.insert(1, 'PPLM')
+# from PPLM import run_pplm, pplm_classification_head
+
+# from run_pplm import run_pplm_example
+from run_pplm import get_bag_of_words_indices, set_generic_model_params, get_classifier, generate_text_pplm
+from run_pplm import DISCRIMINATOR_MODELS_PARAMS, PPLM_BOW_DISCRIM, PPLM_BOW, PPLM_DISCRIM
+from transformers import GPT2LMHeadModel, GPT2TokenizerFast
+from transformers import GPT2Tokenizer
+import torch
+import numpy as np
+
+# model = GPT2LMHeadModel.from_pretrained(
+#     "C:/Users/Admin/simple_text_generation/mygpt2-medium",
+#     output_hidden_states=True)
+#
+
+QUIET = 0
+REGULAR = 1
+VERBOSE = 2
+VERY_VERBOSE = 3
+VERBOSITY_LEVELS = {
+    'quiet': QUIET,
+    'regular': REGULAR,
+    'verbose': VERBOSE,
+    'very_verbose': VERY_VERBOSE,
+}
+
+
+def full_text_generation(
+        model,
+        tokenizer,
+        context=None,
+        num_samples=1,
+        device="cuda",
+        bag_of_words=None,
+        discrim=None,
+        class_label=None,
+        length=100,
+        stepsize=0.02,
+        temperature=1.0,
+        top_k=10,
+        sample=True,
+        num_iterations=3,
+        grad_length=10000,
+        horizon_length=1,
+        window_length=0,
+        decay=False,
+        gamma=1.5,
+        gm_scale=0.9,
+        kl_scale=0.01,
+        verbosity_level=REGULAR,
+        **kwargs
+):
+    classifier, class_id = get_classifier(
+        discrim,
+        class_label,
+        device
+    )
+
+    unpert_gen_tok_text, _, _ = generate_text_pplm(
+        model=model,
+        tokenizer=tokenizer,
+        context=context,
+        device=device,
+        length=length,
+        sample=sample,
+        perturb=False,
+        verbosity_level=verbosity_level
+    )
+
+    bow_indices = []
+    if bag_of_words:
+        bow_indices = get_bag_of_words_indices(bag_of_words.split(";"),
+                                               tokenizer)
+
+    if bag_of_words and classifier:
+        loss_type = PPLM_BOW_DISCRIM
+        if verbosity_level >= REGULAR:
+            print("Both PPLM-BoW and PPLM-Discrim are on. "
+                  "This is not optimized.")
+
+    elif bag_of_words:
+        loss_type = PPLM_BOW
+        if verbosity_level >= REGULAR:
+            print("Using PPLM-BoW")
+
+    elif classifier is not None:
+        loss_type = PPLM_DISCRIM
+        if verbosity_level >= REGULAR:
+            print("Using PPLM-Discrim")
+
+    else:
+        # TODO: Try to prepare something better
+        if device == 'cuda':
+            torch.cuda.empty_cache()
+        return unpert_gen_tok_text, None, None, None # pert_gen_tok_texts, discrim_losses, losses_in_time
+        raise Exception("Specify either a bag of words or a discriminator")
+
+
+    if device == 'cuda':
+        torch.cuda.empty_cache()
+
+    pert_gen_tok_texts = []
+    discrim_losses = []
+    losses_in_time = []
+
+    for i in range(num_samples):
+        pert_gen_tok_text, discrim_loss, loss_in_time = generate_text_pplm(
+            model=model,
+            tokenizer=tokenizer,
+            context=context,
+            device=device,
+            perturb=True,
+            bow_indices=bow_indices,
+            classifier=classifier,
+            class_label=class_id,
+            loss_type=loss_type,
+            length=length,
+            stepsize=stepsize,
+            temperature=temperature,
+            top_k=top_k,
+            sample=sample,
+            num_iterations=num_iterations,
+            grad_length=grad_length,
+            horizon_length=horizon_length,
+            window_length=window_length,
+            decay=decay,
+            gamma=gamma,
+            gm_scale=gm_scale,
+            kl_scale=kl_scale,
+            verbosity_level=verbosity_level
+        )
+        pert_gen_tok_texts.append(pert_gen_tok_text)
+        if classifier is not None:
+            discrim_losses.append(discrim_loss.data.cpu().numpy())
+        losses_in_time.append(loss_in_time)
+
+    if device == 'cuda':
+        torch.cuda.empty_cache()
+
+    return unpert_gen_tok_text, pert_gen_tok_texts, discrim_losses, losses_in_time
+
+def  run_my_pplm(
+        # pretrained_model="gpt2-medium",
+        model,
+        tokenizer,
+        cond_text="",
+        uncond=False,
+        num_samples=1,
+        bag_of_words=None,
+        discrim=None,
+        discrim_weights=None,
+        discrim_meta=None,
+        class_label=-1,
+        length=100,
+        stepsize=0.02,
+        temperature=1.0,
+        top_k=10,
+        sample=True,
+        num_iterations=3,
+        grad_length=10000,
+        horizon_length=1,
+        window_length=0,
+        decay=False,
+        gamma=1.5,
+        gm_scale=0.9,
+        kl_scale=0.01,
+        seed=0,
+        no_cuda=False,
+        colorama=False,
+        verbosity='regular'
+):
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+
+
+    # set verbosiry
+    verbosity_level = VERBOSITY_LEVELS.get(verbosity.lower(), REGULAR)
+
+    if discrim == 'generic':
+        set_generic_model_params(discrim_weights, discrim_meta)
+
+    if discrim is not None:
+        discriminator_pretrained_model = DISCRIMINATOR_MODELS_PARAMS[discrim][
+            "pretrained_model"
+        ]
+        if pretrained_model != discriminator_pretrained_model:
+            pretrained_model = discriminator_pretrained_model
+            if verbosity_level >= REGULAR:
+                print("discrim = {}, pretrained_model set "
+                "to discriminator's = {}".format(discrim, pretrained_model))
+
+    # # load pretrained model
+    # model = GPT2LMHeadModel.from_pretrained(
+    #     pretrained_model,
+    #     output_hidden_states=True
+    # )
+
+    # tokenizer = GPT2Tokenizer.from_pretrained(pretrained_model)
+    # figure out conditioning text
+    if uncond:
+        tokenized_cond_text = tokenizer.encode(
+            [tokenizer.bos_token],
+            add_special_tokens=False
+        )
+    else:
+        raw_text = cond_text
+        while not raw_text:
+            print("Did you forget to add `--cond_text`? ")
+            raw_text = input("Model prompt >>> ")
+        tokenized_cond_text = tokenizer.encode(
+            tokenizer.bos_token + raw_text,
+            add_special_tokens=False
+        )
+
+
+    device = "cuda" if torch.cuda.is_available() and not no_cuda else "cpu"
+    model.to(device)
+    model.eval()
+
+    # Freeze GPT-2 weights
+    for param in model.parameters():
+        param.requires_grad = False
+
+    unpert_gen_tok_text, pert_gen_tok_texts, _, _ = full_text_generation(
+        model=model,
+        tokenizer=tokenizer,
+        context=tokenized_cond_text,
+        device=device,
+        num_samples=num_samples,
+        bag_of_words=bag_of_words,
+        discrim=discrim,
+        class_label=class_label,
+        length=length,
+        stepsize=stepsize,
+        temperature=temperature,
+        top_k=top_k,
+        sample=sample,
+        num_iterations=num_iterations,
+        grad_length=grad_length,
+        horizon_length=horizon_length,
+        window_length=window_length,
+        decay=decay,
+        gamma=gamma,
+        gm_scale=gm_scale,
+        kl_scale=kl_scale,
+        verbosity_level=verbosity_level
+    )
+
+    # untokenize unperturbed text
+    # unpert_gen_text = tokenizer.decode(unpert_gen_tok_text.tolist()[0])
+
+    unpert_gen_text = tokenizer.decode(
+        unpert_gen_tok_text.tolist()[0],
+        skip_special_tokens=True,
+        clean_up_tokenization_spaces=False,
+    )
+
+    if verbosity_level >= REGULAR:
+        print("=" * 80)
+
+    print("= Unperturbed generated text =")
+    print(unpert_gen_text)
+
+    generated_texts = []
+    bow_word_ids = set()
+
+    if bag_of_words and colorama:
+        bow_indices = get_bag_of_words_indices(bag_of_words.split(";"),
+                                               tokenizer)
+        for single_bow_list in bow_indices:
+            # filtering all words in the list composed of more than 1 token
+            filtered = list(filter(lambda x: len(x) <= 1, single_bow_list))
+            # w[0] because we are sure w has only 1 item because previous fitler
+            bow_word_ids.update(w[0] for w in filtered)
+
+    # iterate through the perturbed texts
+    if pert_gen_tok_texts:
+        for i, pert_gen_tok_text in enumerate(pert_gen_tok_texts):
+            try:
+                # untokenize unperturbed text
+                if colorama:
+                    import colorama
+
+                    pert_gen_text = ''
+                    for word_id in pert_gen_tok_text.tolist()[0]:
+                        if word_id in bow_word_ids:
+                            # pert_gen_text += '{}{}{}'.format(
+                            #     colorama.Fore.RED,
+                            #     tokenizer.decode([word_id],
+                            #                     skip_special_tokens=True,
+                            #                     clean_up_tokenization_spaces=False,),
+                            #     colorama.Style.RESET_ALL
+                            # )
+                            pert_gen_text += '{}{}{}'.format(
+                                '<span style="color:blue">',
+                                tokenizer.decode([word_id],
+                                                skip_special_tokens=True,
+                                                clean_up_tokenization_spaces=False,),
+                                "</span>"
+                            )
+                        else:
+                            pert_gen_text += tokenizer.decode([word_id],
+                                                            skip_special_tokens=True,
+                                                            clean_up_tokenization_spaces=False,)
+                else:
+                    # pert_gen_text = tokenizer.decode(pert_gen_tok_text.tolist()[0])
+                    pert_gen_text = tokenizer.decode(
+                        pert_gen_tok_text.tolist()[0],
+                        skip_special_tokens=True,
+                        clean_up_tokenization_spaces=False,
+                    )
+                print([tokenizer.decode(bwid) for bwid in bow_word_ids])
+                print("= Perturbed generated text {} =".format(i + 1))
+                print(pert_gen_text)
+                print()
+            except:
+                print("""ERROR in pert""")
+                pass
+
+            # keep the prefix, perturbed seq, original seq for each index
+            # generated_texts.append(
+            #     (unpert_gen_tok_text, tokenized_cond_text, pert_gen_tok_text)
+            # )
+            generated_texts.append(
+                (unpert_gen_text, pert_gen_text)
+            )
+        else:
+            generated_texts.append((unpert_gen_text, ""))
+    return generated_texts
+
+
+
+
+
